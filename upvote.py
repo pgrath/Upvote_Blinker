ad43dafcf3000160f0bf941c580fed7ff917419e
pgrath <hofnbricl@gmail.com>
2016-02-12 15:49:45 -0500

    edited in wrong directory, lol

--- upvote.py
+++ upvote.py
@@ -1,11 +1,26 @@
 import praw
+import time
 
 #set up user agent and select user 
 r = praw.Reddit(user_agent='Upvote_blinker')
 
 usr = r.get_redditor('hofnbricl')
 
-#easy stuff here
-totalKarma = usr.comment_karma + usr.link_karma
+#get the karma
+def getKarma():
+    totalKarma = usr.comment_karma + usr.link_karma
+    return totalKarma
 
-print(totalKarma)
+#get initial karma value
+karmaScore = getKarma()
+
+while True:
+    #get most recent karma score, then inform the user if it is greater than the initial value
+    currScore = getKarma()
+    if karmaScore < currScore:
+        print("Upvote detected!")
+        karmaScore = currScore
+    else:
+        continue
+
+    time.sleep(5)
