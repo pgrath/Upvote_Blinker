73ff3bc21d2fe300c5d3b07145cea2b06b828852
pgrath <p.jmcgrath6@gmail.com>
2016-02-24 04:49:21 +0000

    Added PiGlow code

--- upvote.py
+++ upvote.py
@@ -1,15 +1,35 @@
 import praw
 import time
+import piglow
 
 #set up user agent and select user 
 r = praw.Reddit(user_agent='Upvote_blinker')
+usr = r.get_redditor('abdallhhelles')
 
-usr = r.get_redditor('hofnbricl')
+#set up piglow
+piglow.auto_update = True
+piglow.clear_on_exit = True
 
 #get the karma
 def getKarma():
     totalKarma = usr.comment_karma + usr.link_karma
     return totalKarma
+
+#function to make the piglow glow
+def glow():
+	for p in range(2):
+		for x in range(255):
+			piglow.arm(0, x)
+			piglow.arm(1, x)
+			piglow.arm(2, x)
+
+		for x in reversed(range(255)):
+			piglow.arm(0, x)
+			piglow.arm(1, x)
+			piglow.arm(2, x)
+
+
+
 
 #get initial karma value
 karmaScore = getKarma()
@@ -20,6 +40,7 @@ while True:
     if karmaScore < currScore:
         print("Upvote detected!")
         karmaScore = currScore
+	glow()
     else:
         continue
 
